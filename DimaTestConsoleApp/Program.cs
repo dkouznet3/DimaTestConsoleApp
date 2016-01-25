5521c9ff0aa943de15701d9e6f615db4468ff829
Dima Kouznetsov <dkouznet@gmail.com>
2016-01-25 14:59:41 -0500

    First commit

---
+++ DimaTestConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimaTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
